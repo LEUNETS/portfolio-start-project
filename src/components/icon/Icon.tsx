8a5bbc9e9d55497db4b60cb6da6d3fa62d4c4dee
Dmitry Leunets <leunec@mail.ru>
2024-05-02 21:08:57 +0300

    first commit

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import iconsSprite from '../../assets/images/sprite.svg'
+
+type IconPropsType = {
+    iconId: string
+}
+
+export const Icon = (props: IconPropsType) => {
+  return (
+<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <use xlinkHref={ `${iconsSprite}#${props.iconId}` }/>
+</svg>
+  )
+};
