d2870a4bb32227b73d0bcfc8e98ffa081812de62
Dmitry Leunets <leunec@mail.ru>
2024-05-07 01:06:45 +0300

    lesson2.3

--- src/layout/header/secthion/main/Main.tsx
+++ src/layout/header/secthion/main/Main.tsx
@@ -2,41 +2,91 @@ import React from "react";
 import styled from "styled-components";
 import photo from "./../../../../assets/images/photo.jpg";
 import { FlexWrapper } from "../../../../components/FlexWrapper";
+import { Container } from "../../../../components/Container";
+import { theme } from "../../../../styles/Theme";
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper align={"center"} justify={"space-around"}>
-        <div>
-          <SpanWelcome>Hi There</SpanWelcome>
-          <Name>I am Leunets Dmitriy</Name>
-          <MainTitle>A Web Developer.</MainTitle>
-        </div>
+      <Container>
+        <FlexWrapper align={"center"} justify={"space-between"}>
+          <div>
+            <SmallText>Hi There</SmallText>
+            <Name>
+              I am <span>Leunets Dmitriy</span>
+            </Name>
+            <MainTitle>A Web Developer.</MainTitle>
+          </div>
+          <PhotoWrapper></PhotoWrapper>
 
-        <Photo src={photo} alt="" />
-      </FlexWrapper>
+          <Photo src={photo} alt="" />
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   );
 };
 
+const StyledMain = styled.section`
+  min-height: 100vh;
+  background-color: #1f1f20;
+  display: flex;
+`;
 
-const StyledMain = styled.div`
-    min-height: 100vh;
-    background-color: #6f7071;
-`
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: "";
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -210px;
+    left: 145px; //ручной хардкод.
+    /* top: -24px;
+    left: 24px; */ //так должно быть.
+    z-index: -2; //не сработал.
+  }
+`;
 
 const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  z-index: 1; // не должно быть тут.
+`;
+const MainTitle = styled.h1`
+  font-weight: 400;
+  font-size: 27px;
 `;
 
-const SpanWelcome = styled.span`
-
-`
-const MainTitle = styled.h1`
-
-`
 const Name = styled.h2`
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 700;
+  font-size: 50px;
+  letter-spacing: 0.05em;
+  margin: 10px 0;
 
-`
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
+`;
+
+const SmallText = styled.h2`
+  font-weight: 400;
+  font-size: 14px;
+`;
