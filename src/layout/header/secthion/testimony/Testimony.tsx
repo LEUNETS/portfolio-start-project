3068693aeabbe68c175b8bf54d2d9cb96e98db70
Dmitry Leunets <leunec@mail.ru>
2024-05-11 23:43:20 +0300

    lesson3/5 testimony

--- src/layout/header/secthion/testimony/Testimony.tsx
+++ src/layout/header/secthion/testimony/Testimony.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import { SecthionTitle } from "../../../../components/SecthionTitle";
-import { Icon } from './../../../../components/icon/Icon';
-import { Slider } from "../../../../components/slider/Slider";
-import { FlexWrapper } from "../../../../components/FlexWrapper";
-import { IconWrapper } from "../skill/Skill";
+import {SecthionTitle} from "../../../../components/SecthionTitle";
+import {Icon} from './../../../../components/icon/Icon';
+import {Slider} from "../../../../components/slider/Slider";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
+import {IconWrapper} from "../skill/Skill";
+import {Container} from "../../../../components/Container";
 
 export const Testimony = () => {
-  return (
-    <StyledTestimony>
-      <SecthionTitle>Testimony</SecthionTitle>
-      <FlexWrapper direction={"column"} align={"center"}>
-        <IconWrapper>
-        <Icon iconId={"quote-svg"}/>
-        </IconWrapper>
-    <Slider />
-      </FlexWrapper>
-    </StyledTestimony>
-  );
+    return (
+        <StyledTestimony>
+            <Container>
+                <SecthionTitle>Testimony</SecthionTitle>
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <IconWrapper>
+                        <Icon iconId={"quote-svg"}/>
+                    </IconWrapper>
+                    <Slider/>
+                </FlexWrapper>
+            </Container>
+        </StyledTestimony>
+    );
 };
 
 const StyledTestimony = styled.section`
-background-color: #b1c5d6dc;
-min-height: 50vh;
+    background-color: #b1c5d6dc;
+    min-height: 50vh;
 
-${IconWrapper} {
-  margin: 40px 0 72px;
-}
+    ${IconWrapper} {
+        margin: 40px 0 72px;
+    }
 `
